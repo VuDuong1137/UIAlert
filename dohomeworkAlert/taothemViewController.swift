d058e1c1998976050565304f8447fa4bc98f36d0
Dương chãng <macshop@MacBook-Pro-cua-Duong-nhe.local>
2020-05-07 08:56:12 +0700

    new file view controller

---
+++ dohomeworkAlert/taothemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  taothemViewController.swift
+//  dohomeworkAlert
+//
+//  Created by Dương chãng on 5/7/20.
+//  Copyright © 2020 macshop. All rights reserved.
+//
+
+import UIKit
+
+class taothemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
